62fefaaebf638e73f8c975c02f8e484c9f61e9ac
happy-phiri <Happyphiri94@gmail.com>
2020-07-28 20:08:33 +0200

    improve error handling and add statement on results page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const https = require("https");
 const fetch = require('node-fetch');
+const { json } = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,7 +13,6 @@ app.use(express.static("public"));
 
 //Variables for the country search
 
-let newDeaths = "";
 let countrySearched = "";
 let totalCases = "";
 let totalDeaths = "";
@@ -25,7 +25,6 @@ let activeCases = "";
 let criticalCases = "";
 let totalTests = "";
 let population = "";
-let updateTime = "";
 
 app.get("/", function(req, res){
     res.render("home");
@@ -33,7 +32,7 @@ app.get("/", function(req, res){
 
 app.get("/about", function(req, res){
     res.render("about");
-})
+});
 
 // Statistics For All Continents
 
@@ -62,48 +61,60 @@ app.get("/statistics", function(req, res){
     });
 });
 
- //Code for the country search box
+ //Routes for the country search box
+ app.get("/fail", function(req, res){
+     res.render("fail");
+ });
 
 app.post("/statistics", function(req, res){
     countrySearched = _.upperCase(req.body.searchCountry);
     const url = "https://corona.lmao.ninja/v2/countries/" + countrySearched +"?yesterday&strict&query%20";
 
     https.get(url, function(response){
-
         response.on("data", function(data){
-            const countryCovidData = JSON.parse(data);
-            totalCases = countryCovidData.cases.toLocaleString();
-            countryFlag = countryCovidData.countryInfo.flag;
-            casesToday = countryCovidData.todayCases.toLocaleString();
-            deathsToday = countryCovidData.todayDeaths.toLocaleString();
-            recoveredToday = countryCovidData.todayRecovered.toLocaleString();
-            totalDeaths = countryCovidData.deaths.toLocaleString();
-            activeCases = countryCovidData.active.toLocaleString();
-            criticalCases = countryCovidData.critical.toLocaleString();
-            totalTests = countryCovidData.tests.toLocaleString();
-            totalRecovered = countryCovidData.recovered.toLocaleString();
-            population = countryCovidData.population.toLocaleString();
-            res.redirect("/results")
+            console.log(response.statusCode);
+
+            if(response.statusCode != 404){
+
+                const countryCovidData = JSON.parse(data);
+                totalCases = countryCovidData.cases.toLocaleString();
+                countryFlag = countryCovidData.countryInfo.flag;
+                casesToday = countryCovidData.todayCases.toLocaleString();
+                deathsToday = countryCovidData.todayDeaths.toLocaleString();
+                recoveredToday = countryCovidData.todayRecovered.toLocaleString();
+                totalDeaths = countryCovidData.deaths.toLocaleString();
+                activeCases = countryCovidData.active.toLocaleString();
+                criticalCases = countryCovidData.critical.toLocaleString();
+                totalTests = countryCovidData.tests.toLocaleString();
+                totalRecovered = countryCovidData.recovered.toLocaleString();
+                population = countryCovidData.population.toLocaleString();
+                res.redirect("/results");
+
+            } else {
+                res.redirect("/fail");
+            }
+               
         });
     });
 });
 
 app.get("/results", function(req, res){
-    res.render("results", {
-        countrySearched: countrySearched, 
-        countryFlag: countryFlag,
-        population: population,
-        totalCases: totalCases,
-        totalDeaths: totalDeaths,
-        totalTests: totalTests,
-        totalRecovered: totalRecovered,
-        casesToday: casesToday,
-        deathsToday: deathsToday,
-        recoveredToday: recoveredToday,
-        activeCases: activeCases,
-        criticalCases : criticalCases
-    });
+        res.render("results", {
+            countrySearched: countrySearched, 
+            countryFlag: countryFlag,
+            population: population,
+            totalCases: totalCases,
+            totalDeaths: totalDeaths,
+            totalTests: totalTests,
+            totalRecovered: totalRecovered,
+            casesToday: casesToday,
+            deathsToday: deathsToday,
+            recoveredToday: recoveredToday,
+            activeCases: activeCases,
+            criticalCases : criticalCases
+        });
 });
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000");
